3cfa36b5a3b32760c8af03e15f3a91fae0141a48
Gábor Sápi <gaborsapi9999@gmail.com>
2021-05-17 19:35:21 +0200

    Players are stored in a database and first 10 players shows off in the leaderboard.

--- src/main/java/boardgame/jdbi/ResultsDAO.java
+++ src/main/java/boardgame/jdbi/ResultsDAO.java
@@ -1,7 +1,23 @@
 package boardgame.jdbi;
 
 
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
+@RegisterBeanMapper(PlayerInfo.class)
 public interface ResultsDAO {
-
+    @SqlUpdate("CREATE TABLE results(name VARCHAR PRIMARY KEY, winCount INTEGER, loseCount INTEGER)")
+    void createTable();
+    @SqlUpdate("INSERT INTO results VALUES(:name,0,0)")
+    void insertPlayer(@Bind("name") String name);
+    @SqlUpdate("UPDATE results SET winCount = winCount + 1 WHERE name = :name")
+    void raisePlayerWin(@Bind("name") String name);
+    @SqlUpdate("UPDATE results SET loseCount = loseCount + 1 WHERE name = :name")
+    void raisePlayerLose(@Bind("name") String name);
+    @SqlQuery("SELECT EXISTS (SELECT * FROM results WHERE name = (:name))")
+    boolean playerExists(@Bind("name") String name);
+    @SqlQuery("SELECT name, winCount, loseCount FROM results ORDER BY winCount DESC, loseCount LIMIT 10")
+    PlayerInfo[] getFirstTenPlayers();
 }
