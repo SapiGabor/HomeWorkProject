e74ebe7fa286e212fd7a6683d561a57f5ccfe447
Gábor Sápi <gaborsapi9999@gmail.com>
2021-05-17 15:02:53 +0200

    Changes in scene handling.

--- src/main/java/boardgame/menu/MenuApplication.java
+++ src/main/java/boardgame/menu/MenuApplication.java
@@ -1,4 +1,19 @@
 package boardgame.menu;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MenuApplication {
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/menu.fxml"));
+        stage.setTitle("JavaFX Board Game Project");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
