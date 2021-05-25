015ae819828190f286a1049404689b9e18ef7ec1
SapiGabor <gaborsapi9999@gmail.com>
2021-05-25 20:27:30 +0200

    Updated test for boardgamemodel.

--- src/test/java/boardgame/model/BoardGameModelTest.java
+++ src/test/java/boardgame/model/BoardGameModelTest.java
@@ -1,16 +1,19 @@
 package boardgame.model;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.OptionalInt;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardGameModelTest {
 
-    static BoardGameModel model;
+    BoardGameModel model;
 
-    @BeforeAll
-    static void init(){
+    @BeforeEach
+    void init(){
         model = new BoardGameModel();
     }
 
@@ -42,21 +45,9 @@ class BoardGameModelTest {
         assertTrue(model.isValidMove(0, KingDirection.DOWN));
         assertTrue(model.isValidMove(1, KingDirection.DOWN));
         assertFalse(model.isValidMove(0, KingDirection.LEFT));
-    }
-
-    @Test
-    void getWhiteHasMoves() {
-        assertTrue(model.getWhiteHasMoves());
-    }
-
-    @Test
-    void getBlackHasMoves() {
-        assertTrue(model.getBlackHasMoves());
-    }
-
-    @Test
-    void isGameOver() {
-        assertFalse(model.isGameOver());
+        assertThrows(IllegalArgumentException.class, () -> {model.isValidMove(-1, KingDirection.DOWN);});
+        model.removeSquare(new Position(1, 0));
+        assertFalse(model.isValidMove(0, KingDirection.UP));
     }
 
     @Test
@@ -69,4 +60,77 @@ class BoardGameModelTest {
     void getPiecePositions() {
         assertEquals(new Position(2,0), model.getPiecePositions().get(0));
     }
+
+    @Test
+    void move() {
+        assertEquals(new Position(2,0 ), model.getPiecePosition(0));
+        model.move(0, KingDirection.DOWN);
+        assertEquals(new Position(3,0 ), model.getPiecePosition(0));
+    }
+
+    @Test
+    void removeSquare() {
+        assertFalse(model.isRemoved(new Position(0, 0)));
+        model.removeSquare(new Position(0, 0));
+        assertTrue(model.isRemoved(new Position(0, 0)));
+    }
+
+    @Test
+    void getRemovableSquares() {
+        assertTrue(model.getRemovableSquares().contains(new Position(0,0)));
+        model.removeSquare(new Position(0, 0));
+        assertFalse(model.getRemovableSquares().contains(new Position(0,0)));
+    }
+
+    @Test
+    void isRemoved() {
+        assertFalse(model.isRemoved(new Position(0, 0)));
+        model.removeSquare(new Position(0, 0));
+        assertTrue(model.isRemoved(new Position(0, 0)));
+    }
+
+    @Test
+    void getAllPiecePositions() {
+        assertEquals(2, model.getAllPiecePositions().size());
+    }
+
+    @Test
+    void getPieceNumber() {
+        assertEquals(1, model.getPieceNumber(new Position(3, 7)).getAsInt());
+        assertEquals(OptionalInt.empty(), model.getPieceNumber(new Position(0, 0)));
+    }
+
+    @Test
+    void getWhiteHasMoves() {
+        assertTrue(model.getWhiteHasMoves());
+        model.removeSquare(new Position(1, 0));
+        model.removeSquare(new Position(1, 1));
+        model.removeSquare(new Position(2,1));
+        model.removeSquare(new Position(3,1));
+        model.removeSquare(new Position(3,0));
+        assertFalse(model.getWhiteHasMoves());
+    }
+
+    @Test
+    void getBlackHasMoves() {
+        assertTrue(model.getBlackHasMoves());
+        model.removeSquare(new Position(2, 7));
+        model.removeSquare(new Position(2, 6));
+        model.removeSquare(new Position(3,6));
+        model.removeSquare(new Position(4,6));
+        model.removeSquare(new Position(4,7));
+        assertFalse(model.getBlackHasMoves());
+    }
+
+    @Test
+    void isGameOver() {
+        assertFalse(model.isGameOver());
+        model.removeSquare(new Position(2, 7));
+        model.removeSquare(new Position(2, 6));
+        model.removeSquare(new Position(3,6));
+        model.removeSquare(new Position(4,6));
+        model.removeSquare(new Position(4,7));
+        assertTrue(model.isGameOver());
+    }
+
 }
