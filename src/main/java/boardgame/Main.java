3d83c4c6dd5fb041b70bdef6b28771160727f61c
SapiGabor <gaborsapi9999@gmail.com>
2021-05-25 20:03:34 +0200

    Updated javadoc.

--- src/main/java/boardgame/Main.java
+++ src/main/java/boardgame/Main.java
@@ -3,12 +3,12 @@ package boardgame;
 import boardgame.menu.MenuApplication;
 import javafx.application.Application;
 /**
- * Main class of the application
+ * Main class of the application.
  */
 public class Main {
     /**
-     * Launches {@code MainMenuApplication}
-     * @param args currently not using
+     * Launches {@code MainMenuApplication}.
+     * @param args currently not using.
      */
     public static void main(String[] args) {
         Application.launch(MenuApplication.class, args);
